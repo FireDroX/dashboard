edec2b9a915539b471802cf7fd30c4a81e0f34a4
FireDroX <firedrox@gmail.com>
2026-08-02 12:00:52 +0200

    feat: add @nestjs/serve-static for serving static files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'node:path';
 import { MonitorsModule } from './monitors/monitors.module';
 
 @Module({
   imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'client', 'dist'),
+    }),
+
     ConfigModule.forRoot({
       isGlobal: true,
     }),
